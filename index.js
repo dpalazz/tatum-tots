fc23522551761a87b749e8d8ed3b35b827880ece
Dana Palazzo <dana@fretless.com>
2018-05-17 10:46:45 -0400

    Store the flick in an object and add id

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const app = {
   init: function(formSelector) {
+    this.max = 0
     document
       .querySelector(formSelector)
-      .addEventListener('submit', this.handleSubmit)
+      .addEventListener('submit', ev => {
+        ev.preventDefault()
+        this.handleSubmit(ev)
+      })
   },
-  handleSubmit: function(ev){
-    ev.preventDefault()
-    const movieName = ev.target.flickName.value    
+  handleSubmit: function(ev) {
+    const movieName = ev.target.flickName.value
+    const flick = {
+      id: ++this.max,
+      name: movieName,
+    }
+    console.log(flick)
+    ev.target.reset()
   },
 }
 
